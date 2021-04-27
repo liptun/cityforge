9d542ecae5562b1a5c20dbbd07fc0efa58da6439
Rafał Karczmarzyk <liptunred@gmail.com>
2021-04-27 19:47:50 +0200

    user controls

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import Game from './Game/Game'
 
 function App() {
     const [game, setGame] = useState<Game>()
-    const [seed, setSeed] = useState<string>('')
+    const [seed, setSeed] = useState<string>('cityforge')
+    const [seaLevel, setSeaLevel] = useState<number>(0)
+    const [roughness, setRoughness] = useState<number>(0.2)
+    const [mountainess, setMountainess] = useState<number>(2)
+    const [debugNoise, setDebugNoise] = useState<boolean>(false)
 
     useEffect(() => {
         const gameInstance = new Game()
@@ -13,14 +17,64 @@ function App() {
         }
     }, [])
 
+    useEffect(() => {
+        generateMapHandle()
+    }, [seed, seaLevel, roughness, mountainess, debugNoise])
+
     const generateMapHandle = useCallback(() => {
-        game?.generateMap(seed)
-    }, [game, seed])
+        game?.generateMap({ seed, seaLevel, roughness, mountainess, debugNoise })
+    }, [game, seed, seaLevel, roughness, mountainess, debugNoise])
 
     return (
         <div className="App">
-            <input type="text" placeholder="seed" onChange={(e) => setSeed(e.currentTarget.value)} />
-            <button onClick={generateMapHandle}>Generate</button>
+            <div className="Sidebar">
+                <div className="Option">
+                    <p>Seed</p>
+                    <input type="text" onChange={(e) => setSeed(e.currentTarget.value)} value={seed} />
+                </div>
+                <div className="Option">
+                    <p>Sea Level: {seaLevel}</p>
+                    <input
+                        type="number"
+                        min="-1"
+                        max="1"
+                        step={0.1}
+                        value={seaLevel}
+                        onChange={(e) => setSeaLevel(Number(e.currentTarget.value))}
+                    />
+                </div>
+                <div className="Option">
+                    <p>Roughness: {roughness}</p>
+                    <input
+                        type="number"
+                        min="0"
+                        max="1"
+                        step={0.1}
+                        value={roughness}
+                        onChange={(e) => setRoughness(Number(e.currentTarget.value))}
+                    />
+                </div>
+                <div className="Option">
+                    <p>Mountainess: {mountainess}</p>
+                    <input
+                        type="number"
+                        min="0"
+                        max="5"
+                        step={0.1}
+                        value={mountainess}
+                        onChange={(e) => setMountainess(Number(e.currentTarget.value))}
+                    />
+                </div>
+                <div className="Option">
+                    <p>Toggle debug noise</p>
+                    <button onClick={() => {
+                        setDebugNoise(!debugNoise)
+                    }}>{debugNoise ? 'enabled' : 'disabled'}</button>
+                </div>
+                <div className="Option">
+                    <button onClick={generateMapHandle}>Generate</button>
+                </div>
+            </div>
             <canvas id="game"></canvas>
         </div>
     )
--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -6,9 +6,14 @@ import Tile from './Tile'
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
 
 const MAP_SIZE = 256
-const SEA_LEVEL = 0
-const ROUGHNESS = 0.2
-const MOUNTAINS = 2
+
+interface generatorOptions {
+    seed?: string
+    seaLevel?: number
+    roughness?: number
+    mountainess?: number
+    debugNoise?: boolean
+}
 
 class Game {
     app: PIXI.Application | null = null
@@ -34,45 +39,66 @@ class Game {
         })
     }
 
-    generateMap(seed = '2137') {
-        const noise = new SimplexNoise(seed)
-        const noise2 = new SimplexNoise(seed + 'extrasalt')
-        const noise3 = new SimplexNoise(seed + 'extrasaltextra')
-        const noise4 = new SimplexNoise(seed + 'extrasaltextrathin')
-        this.app?.stage.removeChildren()
+    generateMap({
+        seed = 'cityforge',
+        seaLevel = 0,
+        roughness = 0.2,
+        mountainess = 2,
+        debugNoise = false,
+    }: generatorOptions = {}) {
+        console.log('starting generate map')
+        console.time()
+
+        const seed1 = seed ?? ''
+        const seed2 = seed ? seed + '1' : ''
+        const seed3 = seed ? seed + '2' : ''
+        const seed4 = seed ? seed + '3' : ''
+
+        const noise = new SimplexNoise(seed1)
+        const noise2 = new SimplexNoise(seed2)
+        const noise3 = new SimplexNoise(seed3)
+        const noise4 = new SimplexNoise(seed4)
+        this.app?.stage?.removeChildren?.()
         for (let y = 0; y < MAP_SIZE; y++) {
             for (let x = 0; x < MAP_SIZE; x++) {
                 const mapTile = new Tile(x * this.tileSize, y * this.tileSize)
                 const noiseValue1 = (noise.noise2D(x / 300, y / 300) + 1) / 2
-                const noiseValue2 = noise2.noise2D(x / 44, y / 44) * .2
-                const noiseValue3 = noise3.noise2D(x / 16, y / 16) * ROUGHNESS + 1
-                const noiseValue4 = noise4.noise2D(x / 150, y / 150) * MOUNTAINS + 1
+                const noiseValue2 = noise2.noise2D(x / 44, y / 44) * 0.2
+                const noiseValue3 = noise3.noise2D(x / 16, y / 16) * roughness + 1
+                const noiseValue4 = noise4.noise2D(x / 150, y / 150) * mountainess + 1
 
-                let noiseValueStack = SEA_LEVEL
+                let noiseValueStack = seaLevel * -1
                 noiseValueStack += noiseValue1
                 noiseValueStack += noiseValue2
                 noiseValueStack *= noiseValue3
                 noiseValueStack *= noiseValue4
+                noiseValueStack /= 1.2
 
                 const clampNoiseValue = Math.min(1, Math.max(0, noiseValueStack))
-                mapTile.transformToWater()
-                // mapTile.alpha = clampNoiseValue
-                if (clampNoiseValue > 0.33) {
-                    mapTile.transformToSand()
-                }
-                if (clampNoiseValue > 0.4) {
-                    mapTile.transformToLand()
-                }
-                if (clampNoiseValue > 0.8) {
-                    mapTile.transformToDirt()
-                }
-                if (clampNoiseValue > 0.95) {
-                    mapTile.transformToRock()
+
+                if (debugNoise) {
+                    mapTile.alpha = clampNoiseValue
+                } else {
+                    mapTile.transformToWater()
+                    if (clampNoiseValue > 0.33) {
+                        mapTile.transformToSand()
+                    }
+                    if (clampNoiseValue > 0.4) {
+                        mapTile.transformToLand()
+                    }
+                    if (clampNoiseValue > 0.8) {
+                        mapTile.transformToDirt()
+                    }
+                    if (clampNoiseValue > 0.95) {
+                        mapTile.transformToRock()
+                    }
                 }
 
                 this.app?.stage.addChild(mapTile)
             }
         }
+        
+        console.timeEnd()
     }
 
     kill() {
