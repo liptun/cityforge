52c68145959f02227c1e2999f02d993254fb6a01
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2021-10-22 21:07:09 +0200

    replace sass with styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,70 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.scss'
+import { createGlobalStyle } from 'styled-components'
+// import './index.scss'
 import App from './App'
+
+const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap');
+
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    body {
+        background-color: #333;
+    }
+
+    canvas {
+        display: block;
+        @media (max-width: 767px) {
+            width: 100%;
+            margin: auto;
+        }
+    }
+
+    .App {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .Sidebar {
+        width: 300px;
+        background-color: .7;
+        padding: 30px;
+        @media (max-width: 767px) {
+            width: 100%;
+            padding: 15px;
+        }
+    }
+    .Option {
+        display: grid;
+        grid-gap: 10px;
+        color: white;
+        padding: 8px 0;
+        p {
+            font-size: 1.2em;
+            font-weight: 300;
+        }
+        button {
+            border: 0;
+            display: block;
+            padding: 5px;
+            background-color: #111;
+            color: white;
+            cursor: pointer;
+            &:hover {
+                background-color: rgb(224, 71, 0);
+            }
+        }
+    }
+`
 
 ReactDOM.render(
     <React.StrictMode>
+        <GlobalStyles />
         <App />
     </React.StrictMode>,
     document.getElementById('root')
